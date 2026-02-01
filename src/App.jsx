7a22b6cef3b9d2c8ab2f6cd6441208593003b8ed
Shivani Barman <shivanibarman247@gmail.com>
2026-02-01 12:51:11 +0530

    implement login functionality with form handling and state management

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,10 @@
 import React from 'react'
+import Login from './components/Auth/Login';
 
 const App = () => {
   return (
     <>
-      <div className="flex h-screen w-screen items-center justify-center">
-        <div className="border-2 rounded-xl border-emerald-800 p-20">
-          <form className="flex flex-col items-center justify-center">
-            <input
-              required
-              className=" text-white outline-none bg-transparent border-2 border-emerald-600 rounded-full py-3 px-5 text-xl placeholder:text-gray-400"
-              type="email"
-              placeholder="Enter email:"
-            />
-
-            <input
-              required
-              className=" text-white outline-none bg-transparent border-2 border-emerald-600 rounded-full py-3 px-5 text-xl mt-3 placeholder:text-gray-400"
-              type="password"
-              placeholder="Enter password:"
-            />
-
-            <button className=" mt-5 text-white outline-none border-none bg-emerald-600 rounded-full py-3 px-5 text-xl placeholder:text-gray-400">
-              Login
-            </button>
-          </form>
-        </div>
-      </div>
+      {/* <Login/> */}
     </>
   );
 }
--- src/components/Auth/Login.jsx
+++ src/components/Auth/Login.jsx
@@ -1,10 +1,53 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Login = () => {
+
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  const submitHandler = (e) => {
+    e.preventDefault()
+    console.log("email is", email)
+    console.log("password is", password);
+
+    setEmail("")
+    setPassword("")
+  }
+
   return (
-    <div>
-      <Login/>
-    </div>
+      <div className="flex h-screen w-screen items-center justify-center">
+        <div className="border-2 rounded-xl border-emerald-800 p-20">
+        <form
+          onSubmit={(e)=>{submitHandler(e)}}
+          className="flex flex-col items-center justify-center">
+          <input
+            value={email}
+            onChange={(e) => {
+              setEmail(e.target.value)
+            }}
+              required
+              className=" text-white outline-none bg-transparent border-2 border-emerald-600 rounded-full py-3 px-5 text-xl placeholder:text-gray-400"
+              type="email"
+              placeholder="Enter email:"
+            />
+
+          <input
+            value={password}
+            onChange={(e) => {
+              setPassword(e.target.value)
+            }}
+              required
+              className=" text-white outline-none bg-transparent border-2 border-emerald-600 rounded-full py-3 px-5 text-xl mt-3 placeholder:text-gray-400"
+              type="password"
+              placeholder="Enter password:"
+            />
+
+            <button className=" mt-5 text-white outline-none border-none bg-emerald-600 rounded-full py-3 px-5 text-xl placeholder:text-gray-400">
+              Login
+            </button>
+          </form>
+        </div>
+      </div>
   )
 }
 

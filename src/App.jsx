be5d1d4204d45a122198c4468548f06696792376
Shivani Barman <shivanibarman247@gmail.com>
2026-02-02 22:52:54 +0530

    implement user authentication with login functionality; remove unused context files

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,34 @@ import Login from './components/Auth/Login';
 import EmployeeDashboard from './components/Dashboard/EmployeeDashboard';
 import AdminDashboard from './components/Dashboard/AdminDashboard';
 import { getLocalStorage, setLocalStorage } from './utils/localStorage';
+import { useState } from 'react';
 
 
 const App = () => {
+
+  const [user, setUser] = useState(null)
+
+  const handleLogin = (email,password) => {
+    if (email == 'admin@me.com' && password == '123') {
+      setUser('admin')
+    }
+    else if (email == "user@me.com" && password == "123") {
+      setUser('employee')
+    } else {
+      alert("Invalid credentials");
+    }
+  }
+
 
   useEffect(() => {
     setLocalStorage()
     getLocalStorage()
   })
+
   return (
     <>
-      <Login/> 
+      {!user ? <Login handleLogin={handleLogin} /> : ''}
+      {user == 'admin' ? <AdminDashboard/> : <EmployeeDashboard/>}
 
       {/* <EmployeeDashboard/> */}
 

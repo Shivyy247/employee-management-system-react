4b88b174debeabb63a4a8ec9beb0fde6ca784210
Shivani Barman <shivanibarman247@gmail.com>
2026-02-08 21:31:07 +0530

    refactor AdminDashboard and EmployeeDashboard components; pass user data to Header for personalized greeting

--- src/components/Dashboard/EmployeeDashboard.jsx
+++ src/components/Dashboard/EmployeeDashboard.jsx
@@ -20,10 +20,21 @@ import Header from "../others/Header";
 import TaskListNumber from "../others/TaskListNumber";
 import TaskList from "../Tasklist/TaskList";
 
+// const EmployeeDashboard = (props) => {
+//   return (
+//     <div className="p-10 bg-[#050505] bg-[radial-gradient(circle_at_top_right,_var(--tw-gradient-stops))] from-emerald-900/10 via-black to-black h-screen overflow-y-auto">
+//       <Header changeUser={props.changeUser} data={props.data} />
+//       <TaskListNumber data={props.data} />
+//       <TaskList data={props.data} />
+//     </div>
+//   );
+// };
+
 const EmployeeDashboard = (props) => {
   return (
     <div className="p-10 bg-[#050505] bg-[radial-gradient(circle_at_top_right,_var(--tw-gradient-stops))] from-emerald-900/10 via-black to-black h-screen overflow-y-auto">
-      <Header changeUser={props.changeUser} data={props.data} />
+      {/* Passing the employee's first name to the header */}
+      <Header changeUser={props.changeUser} data={props.data.firstname} />
       <TaskListNumber data={props.data} />
       <TaskList data={props.data} />
     </div>

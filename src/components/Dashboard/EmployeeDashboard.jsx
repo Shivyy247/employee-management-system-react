9c487a546e90e2c6024255afae5190346c2697e2
Shivani Barman <shivanibarman247@gmail.com>
2026-02-08 20:46:46 +0530

    Refactor task components for improved styling and functionality
    - Updated CompleteTask, FailedTask, and NewTask components with new styles and structure.
    - Enhanced TaskList component to handle task rendering more efficiently.
    - Refactored AllTask and CreateTask components for better user experience and design consistency.
    - Improved Header and TaskListNumber components with updated styles and layout.
    - Added custom scrollbar styles to enhance visual appearance.

--- src/components/Dashboard/EmployeeDashboard.jsx
+++ src/components/Dashboard/EmployeeDashboard.jsx
@@ -1,16 +1,33 @@
-import React from 'react'
-import Header from '../others/Header'
-import TaskListNumber from '../others/TaskListNumber'
-import TaskList from '../Tasklist/TaskList'
+// import React from 'react'
+// import Header from '../others/Header'
+// import TaskListNumber from '../others/TaskListNumber'
+// import TaskList from '../Tasklist/TaskList'
+
+// const EmployeeDashboard = (props) => {
+//   return (
+//     <div className="p-10 bg-[#1C1C1C] h-screen ">
+//       <Header changeUser={props.changeUser} data={props.data} />
+//       <TaskListNumber data={props.data} />
+//       <TaskList data={props.data} />
+//     </div>
+//   );
+// }
+
+// export default EmployeeDashboard
+
+import React from "react";
+import Header from "../others/Header";
+import TaskListNumber from "../others/TaskListNumber";
+import TaskList from "../Tasklist/TaskList";
 
 const EmployeeDashboard = (props) => {
   return (
-    <div className="p-10 bg-[#1C1C1C] h-screen ">
+    <div className="p-10 bg-[#050505] bg-[radial-gradient(circle_at_top_right,_var(--tw-gradient-stops))] from-emerald-900/10 via-black to-black h-screen overflow-y-auto">
       <Header changeUser={props.changeUser} data={props.data} />
       <TaskListNumber data={props.data} />
       <TaskList data={props.data} />
     </div>
   );
-}
+};
 
-export default EmployeeDashboard
+export default EmployeeDashboard;
